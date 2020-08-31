c2c5426eb645efec14d9fe3174cf476ed9065079
Santichai Apirakmaneekul <santichai@innova.co.th>
2020-08-31 08:29:11 +0000

    Add form Loading
    git-tfs-id: [http://tfs.innova.co.th:8080/tfs/japanese]$/BTMT;C25608

---
+++ Inventory/Inventory Record/Loading.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Loading
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Loading))
+        Me.pbLoading = New System.Windows.Forms.PictureBox()
+        CType(Me.pbLoading, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pbLoading
+        '
+        Me.pbLoading.Image = CType(resources.GetObject("pbLoading.Image"), System.Drawing.Image)
+        Me.pbLoading.Location = New System.Drawing.Point(0, 0)
+        Me.pbLoading.Margin = New System.Windows.Forms.Padding(0)
+        Me.pbLoading.Name = "pbLoading"
+        Me.pbLoading.Size = New System.Drawing.Size(200, 200)
+        Me.pbLoading.TabIndex = 0
+        Me.pbLoading.TabStop = False
+        '
+        'Loading
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(200, 200)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.pbLoading)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Loading"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Loading"
+        Me.TopMost = True
+        CType(Me.pbLoading, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pbLoading As PictureBox
+End Class
