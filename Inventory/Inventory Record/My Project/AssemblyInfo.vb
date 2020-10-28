dd5f64d844629611874388be8c52db2497111247
Santichai Apirakmaneekul <santichai@innova.co.th>
2020-10-28 02:05:50 +0000

    Update version to 2.0.0.2
    git-tfs-id: [http://tfs.innova.co.th:8080/tfs/japanese]$/BTMT;C26014

--- Inventory/Inventory Record/My Project/AssemblyInfo.vb
+++ Inventory/Inventory Record/My Project/AssemblyInfo.vb
@@ -9,7 +9,7 @@ Imports System.Runtime.InteropServices
 ' Review the values of the assembly attributes
 
 <Assembly: AssemblyTitle("Inventory Record System")>
-<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyDescription("Manage Master, Calculate and Price")>
 <Assembly: AssemblyCompany("Bridgestone Tire Manufacturing (Thailand) Co., Ltd.")>
 <Assembly: AssemblyProduct("Inventory Record System")>
 <Assembly: AssemblyCopyright("Copyright ©  2020")>
@@ -29,6 +29,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.0.0.1")>
+<Assembly: AssemblyVersion("2.0.0.2")>
 
-<Assembly: AssemblyFileVersion("2.0.0.1")>
+<Assembly: AssemblyFileVersion("2.0.0.2")>
