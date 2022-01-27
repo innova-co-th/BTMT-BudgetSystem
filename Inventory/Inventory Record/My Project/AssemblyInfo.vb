12845348e869ad42b964cc61c59872bf4672cb27
Santichai Apirakmaneekul <santichai@innova.co.th>
2022-01-27 15:16:53 +0000

    Update version to 2.0.0.15 Update publish version to 2.0.0.15
    git-tfs-id: [http://tfs.innova.co.th:8080/tfs/japanese]$/BTMT;C31913

--- Inventory/Inventory Record/My Project/AssemblyInfo.vb
+++ Inventory/Inventory Record/My Project/AssemblyInfo.vb
@@ -29,6 +29,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2.0.0.14")>
+<Assembly: AssemblyVersion("2.0.0.15")>
 
-<Assembly: AssemblyFileVersion("2.0.0.14")>
+<Assembly: AssemblyFileVersion("2.0.0.15")>
