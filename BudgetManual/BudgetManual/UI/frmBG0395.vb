2ae7dc91a8b686c5445df7a6805d70acd232ce69
Pariwat Rattanaprarom <pariwat@innova.co.th>
2022-12-08 13:30:59 +0700

    All MTP to MBP in fromBG0395.vb
    1.Change strTemp " MTP Budget " to " MBP Budget "
    2.Change Text.Contains() "MTP" to "MBP"

--- BudgetManual/BudgetManual/UI/frmBG0395.vb
+++ BudgetManual/BudgetManual/UI/frmBG0395.vb
@@ -43,7 +43,7 @@ Public Class frmBG0395
                     strTemp = CStr(dr("BUDGET_YEAR")) & " Forecast Budget " & CStr(dr("PROJECT_NO"))
 
                 ElseIf CInt(dr("PERIOD_TYPE")) = enumPeriodType.MBPBudget Then
-                    strTemp = CStr(dr("BUDGET_YEAR")) & " MTP Budget " & CStr(dr("PROJECT_NO"))
+                    strTemp = CStr(dr("BUDGET_YEAR")) & " MBP Budget " & CStr(dr("PROJECT_NO"))
 
                 End If
 
@@ -70,7 +70,7 @@ Public Class frmBG0395
             myClsBG0395BL.PeriodType = CStr(enumPeriodType.EstimateBudget)
         ElseIf cboPeriod.Text.Contains("Forecast") Then
             myClsBG0395BL.PeriodType = CStr(enumPeriodType.ForecastBudget)
-        ElseIf cboPeriod.Text.Contains("MTP") Then
+        ElseIf cboPeriod.Text.Contains("MBP") Then
             myClsBG0395BL.PeriodType = CStr(enumPeriodType.MBPBudget)
         End If
         myClsBG0395BL.ProjectNo = Mid(cboPeriod.Text, cboPeriod.Text.LastIndexOf(" ") + 2, cboPeriod.Text.Length - cboPeriod.Text.LastIndexOf(" "))
@@ -116,7 +116,7 @@ Public Class frmBG0395
             myClsBG0395BL.PeriodType = CStr(enumPeriodType.EstimateBudget)
         ElseIf cboPeriod.Text.Contains("Forecast") Then
             myClsBG0395BL.PeriodType = CStr(enumPeriodType.ForecastBudget)
-        ElseIf cboPeriod.Text.Contains("MTP") Then
+        ElseIf cboPeriod.Text.Contains("MBP") Then
             myClsBG0395BL.PeriodType = CStr(enumPeriodType.MBPBudget)
         End If
         myClsBG0395BL.UserId = p_strUserId
